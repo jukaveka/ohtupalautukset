de46463d0fb2527eec604777ca86e18e21be4542
Juho Kavekari <juhokavekari@hotmail.com>
2024-12-02 20:58:18 +0200

    Refactor calculator to use more command-style design pattern

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -16,3 +16,29 @@ class Sovelluslogiikka:
 
     def arvo(self):
         return self._arvo
+
+class Operaatio():
+    def __init__(self, sovelluslogiikka, arvo):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._arvo = arvo 
+
+class Summa(Operaatio):
+    def __init__(self, sovelluslogiikka, arvo):
+        super().__init__(sovelluslogiikka, arvo)
+
+    def suorita(self):
+        self._sovelluslogiikka.plus(self._arvo())
+
+class Erotus(Operaatio):
+    def __init__(self, sovelluslogiikka, arvo):
+        super().__init__(sovelluslogiikka, arvo)
+
+    def suorita(self):
+        self._sovelluslogiikka.miinus(self._arvo())
+
+class Nollaus(Operaatio):
+    def __init__(self, sovelluslogiikka, arvo):
+        super().__init__(sovelluslogiikka, arvo)
+    
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
