a5b28845f3e99c8001f3d4e24e94137423de7c7f
Juho Kavekari <juhokavekari@hotmail.com>
2024-12-02 23:44:56 +0200

    Add reverse functionality to laskin

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,6 +1,7 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edellinen_arvo = 0
 
     def miinus(self, operandi):
         self._arvo = self._arvo - operandi
@@ -17,16 +18,26 @@ class Sovelluslogiikka:
     def arvo(self):
         return self._arvo
 
+    def historia(self):
+        return self._historia
+
+    def aseta_edellinen_arvo(self):
+        self._edellinen_arvo = self._arvo
+    
+    def edellinen_arvo(self):
+        return self._edellinen_arvo
+
 class Operaatio():
     def __init__(self, sovelluslogiikka, arvo):
         self._sovelluslogiikka = sovelluslogiikka
-        self._arvo = arvo 
+        self._arvo = arvo
 
 class Summa(Operaatio):
     def __init__(self, sovelluslogiikka, arvo):
         super().__init__(sovelluslogiikka, arvo)
 
     def suorita(self):
+        self._sovelluslogiikka.aseta_edellinen_arvo()
         self._sovelluslogiikka.plus(self._arvo())
 
 class Erotus(Operaatio):
@@ -34,6 +45,7 @@ class Erotus(Operaatio):
         super().__init__(sovelluslogiikka, arvo)
 
     def suorita(self):
+        self._sovelluslogiikka.aseta_edellinen_arvo()
         self._sovelluslogiikka.miinus(self._arvo())
 
 class Nollaus(Operaatio):
@@ -41,4 +53,12 @@ class Nollaus(Operaatio):
         super().__init__(sovelluslogiikka, arvo)
     
     def suorita(self):
+        self._sovelluslogiikka.aseta_edellinen_arvo()
         self._sovelluslogiikka.nollaa()
+
+class Kumoa(Operaatio):
+    def __init__(self, sovelluslogiikka, arvo):
+        super().__init__(sovelluslogiikka, arvo)
+    
+    def suorita(self):
+        self._sovelluslogiikka.aseta_arvo(self._sovelluslogiikka.edellinen_arvo())
