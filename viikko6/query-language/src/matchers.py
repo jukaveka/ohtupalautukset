f2bd618840dc8b94030149bc4a47befd87ff9438
Juho Kavekari <juhokavekari@hotmail.com>
2024-12-08 23:22:39 +0200

    Add matchers All, Not and HasFewerThan

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -10,6 +10,23 @@ class And:
         return True
 
 
+class All:
+    def test(self, player):
+        return player
+
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return True
+            
+        return False
+
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -27,3 +44,14 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+    
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
