b220869c3a63fc8640c1ee9331357d719c665a55
Juho Kavekari <juhokavekari@hotmail.com>
2024-12-20 14:15:41 +0200

    Remove commented code

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -19,32 +19,6 @@ def main():
 
         peli.pelaa()
 
-        '''
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            kaksinpeli = peli.pelaaja_vs_pelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = peli.pelaaja_vs_tekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = peli.pelaaja_vs_parempi_tekoaly()
-            haastava_yksinpeli.pelaa()
-        else:
-            break
-        '''
-
 
 if __name__ == "__main__":
     main()
